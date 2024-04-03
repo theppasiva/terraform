11009906b22b54089680e6c2d37a3777f96a1701
theppasiva <theppasiva@gmail.com>
2024-04-03 11:34:59 +0530

    terraform

--- session-01/ec2/provider.tf
+++ session-01/ec2/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.33.0" #aws provider version, not terraform
+      version = "5.43.0" #aws provider version, not terraform
     }
   }
 }
