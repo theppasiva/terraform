d2ccb6ac91c145bdde05a394c66c56a83c846d09
theppasiva <theppasiva@gmail.com>
2024-04-03 19:48:32 +0530

    Terraform

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.roboshop-all.id] #this means list
 
   tags = {
